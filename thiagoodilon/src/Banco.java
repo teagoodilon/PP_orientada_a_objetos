76fac754a978460f6fc0e9f1d4e3b7c478b19338
Thiago Odilon <thiago-almeida05@hotmail.com>
2022-06-22 14:05:05 -0300

    Terminando passo 1.4

--- thiagoodilon/src/Banco.java
+++ thiagoodilon/src/Banco.java
@@ -1,29 +1,61 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Banco {
     Scanner entrada = new Scanner(System.in);
-    private Conta conta;
+    private Conta[] conta = new Conta[2];
 
     public void criarConta() {
-        System.out.println("Insira o nome do cliente:");
+        System.out.println("Insira o nome do cliente 1:");
         String nome = entrada.nextLine();
-        System.out.println("Insira o cpf do cliente:");
+        System.out.println("Insira o nome do cliente 2:");
+        String nome2 = entrada.nextLine();
+        System.out.println("Insira o cpf do cliente 1:");
         String cpf = entrada.nextLine();
-        System.out.println("Insira o limite da conta do cliente:");
+        System.out.println("Insira o cpf do cliente 2:");
+        String cpf2 = entrada.nextLine();
+        System.out.println("Insira o limite da conta do cliente 1:");
         double limite = Double.parseDouble(entrada.nextLine());
-
-        Cliente cliente = new Cliente(nome, cpf);
-        this.conta = new Conta(cliente, limite);
-        System.out.println("Conta criada com sucesso!\n");
+        System.out.println("Insira o limite da conta do cliente 2:");
+        double limite2 = Double.parseDouble(entrada.nextLine());
+        tiposConta(nome, cpf, limite, 0);
+        tiposConta(nome2, cpf2, limite2, 1);
+        consultarSaldo(conta[0].getid());
+        consultarSaldo(conta[1].getid());
     }
 
-    public void consultarSaldo() {
-        if (this.conta != null) {
-            System.out.println("Cliente:");
-            System.out.print(this.conta.getNomeCliente());
-            System.out.println("Saldo em conta: R$");
-            System.out.print(this.conta.getSaldo());
+    public void tiposConta(String nome, String cpf, double limite, int numConta){
+        System.out.println("Deseja depositar um valor inicial em sua conta?");
+        System.out.println("(1) Sim");
+        System.out.println("(2) Não");
+        int comando = Integer.parseInt(entrada.nextLine());
+        switch(comando){
+            case 1:
+                System.out.println("Insira o saldo inicial da conta:");
+                double saldo = Double.parseDouble(entrada.nextLine());
+                Cliente mCliente = new Cliente(nome, cpf);
+                this.conta[numConta] = new Conta(mCliente, limite, saldo);
+                System.out.println("Conta criada com sucesso!\n");
+            break;
+            case 2:
+                Cliente cliente = new Cliente(nome, cpf);
+                this.conta[numConta] = new Conta(cliente, limite);
+                System.out.println("Conta criada com sucesso!\n");
+            break;
+            default:
+                System.out.println("Comando Inválido, tente novamente\n");
+                tiposConta(nome, cpf, limite, numConta);
+                break;
+        }
+    }
 
+    public void consultarSaldo(int idconta) {
+        if (this.conta != null) {
+            System.out.print("Cliente: ");
+            System.out.println(this.conta[idconta].getNomeCliente());
+            System.out.print("Saldo em conta: R$");
+            System.out.println(this.conta[idconta].getSaldo());
         } else {
             System.out.println("A conta ainda não foi criada! \n");
         }
@@ -47,7 +79,7 @@ public class Banco {
             if (this.conta.saque(valorSacado)) {
                 System.out.println("Valor sacado com sucesso\n");
             } else {
-                System.out.println("Não foi possível realizar o saque, você não tem esse limite disponível\n");
+                System.out.println("Não foi possível realizar o saque, porque o cliente você não tem esse limite disponível\n");
             }
         } else {
             System.out.println("A conta ainda não foi criada!\n");
@@ -57,7 +89,7 @@ public class Banco {
     public void menu() {
         boolean sairMenu = false;
         while (!sairMenu) {
-            System.out.println("(1) Criar conta");
+            System.out.println("(1) Criar 2 contas");
             System.out.println("(2) Consultar saldo");
             System.out.println("(3) Depositar");
             System.out.println("(4) Sacar");
--- thiagoodilon/src/Conta.java
+++ thiagoodilon/src/Conta.java
@@ -1,12 +1,28 @@
 public class Conta {
+    static int numContas = 1000;
+    private int id;
     private Cliente cliente;
     private double saldo;
     private double limite;
 
     public Conta(Cliente cliente, double limite) {
+        this.id = numContas + 1;
         this.cliente = cliente;
         this.limite = -limite;
         saldo = 0.0;
+        numContas = this.id;
+    }
+
+    public Conta(Cliente cliente, double limite, double saldo) {
+        this.id = numContas + 1;
+        this.cliente = cliente;
+        this.limite = -limite;
+        this.saldo = saldo;
+        numContas = this.id;
+    }
+
+    public int getid(){
+        return id;
     }
 
     public String getNomeCliente() { // retorna saldo na conta
